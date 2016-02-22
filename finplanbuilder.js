22d6e4c2b577a79326af728f57128e0d0d836c56
carolinecl02 <carolinelund@Caroline-Lunds-MacBook-2.local>
2016-02-22 19:56:36 +0000

    monday updates

--- finplanbuilder.js
+++ finplanbuilder.js
@@ -2,10 +2,13 @@ $(function(){
 
 //when user selects, add class of 'selected'
 $(".card").on('click', function(){
-	$(this).toggleClass("selected");
+	if (event.target.tagName.toLowerCase() !== 'input') {
+		$(this).toggleClass("selected");
+	}
+
 });
 
-//when user clicks next, give all non selected items a class of 'hidden' 
+//when user clicks next, give all non selected items a class of 'hidden'
 $("#nextButton").on('click', function(){
 	//get all the cards, loop through them
  	var cardItems=$(".card");
@@ -27,33 +30,42 @@ $("#nextButton").on('click', function(){
 
 //on the next 'next', show the next calculations
 $("#addButton").on('click', function(){
+
+	$(".planintro h4").text("3. Let's see how you're doing...");
 	$(".totals-needed").slideDown();
 
-	//set the default to zero 
+	//set the default to zero
 	var totalNeeded = 0;
 
+	var selected = $('.card.selected');
+
+	for (var i = 0; i < selected.length; i++) {
+		totalNeeded += parseInt($(selected[i]).find('.goal-amount').val(), 10);
+	}
+
 	$('#totalNeeded').text('£' + totalNeeded);
 
-	var totalNeeded = $(".goal-amount").val();
+	$('#totalNeeded').addClass('bignumber');
 
-	$('#totalNeeded').text('£' + totalNeeded);
+	$("#addButton").hide();
+});
 
-})
-
-	//on update button click, compare what they have put in to whatever 
+	//on update button click, compare what they have put in to how much they have
 	$("#updateButton").on('click', function(){
-
-		$(".sadoutcome").slideDown();
 
 		var totalHave = $('#totalHave').val();
 		totalHave = parseFloat(totalHave);
-		totalResult = totalHave - totalNeeded; 
-		$('#totalResult').text('£' + totalResult.toFixed(2));
-	})
+		newTotal = parseInt($('#totalNeeded').text().replace('£', ''));
 
-	//find how much they put in into each goal 
-	
-	//add up the totals 
+		if (totalHave > newTotal) {
+			$(".happyoutcome").slideDown();
+		}
+ 		else {
+			$(".sadoutcome").slideDown();
+		};
+
+	});
+
 
 
 });
